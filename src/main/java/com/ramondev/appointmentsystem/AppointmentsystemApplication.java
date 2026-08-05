92e842482c049c88195c3b147d46f2eea86023ea
Ramon Oliveira <ramonnnoliveirasilva@gmail.com>
2026-08-05 13:26:13 -0300

    first commit

---
+++ src/main/java/com/ramondev/appointmentsystem/AppointmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.ramondev.appointmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramondev/appointmentsystem/AppointmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramondev.appointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
